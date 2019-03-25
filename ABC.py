ca99264ae702770b9d9e0d3a17da8a0d8c9ee825
Amitrajit Bose <amitrajitbose9@gmail.com>
2019-03-25 19:53:06 +0530

    Add blob for future reference

---
+++ ABC.py
@@ -0,0 +1,19 @@
+'''
+Follow this format for making contributions to encryption algorithms
+'''
+
+class ABC:
+	def __init__(self):
+		pass
+
+	def encrypt(self, password, key):
+		'''
+		your encryption algorithm goes here
+		'''
+		return encrypted_password
+
+	def decrypt(self, encrypted_password, key):
+		'''
+		your decryption algorithm goes here
+		'''
+		return decrypted_password
